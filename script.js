b0528aa9dc0956acc67479a3068cf073230b1a6d
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-01-06 15:04:24 -0400

    Create classes Entertainment, Getaway and Nightclub

--- script.js
+++ script.js
@@ -11,8 +11,36 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputPrice = document.querySelector('.form__input--price');
 const inputDances = document.querySelector('.form__input--nightclub-dances');
 
+
+class Entertainment {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
+
+  constructor(coords, cost, duration) {
+    this.coords = coords;
+    this.cost = cost;
+    this.duration = duration;
+  }
+}
+
+class Getaway extends Entertainment {
+  constructor(coords, cost, duration, activityName, activity) {
+    super(coords, cost, duration);
+    this.activityName = activityName;
+    this.activity = activity;
+  }
+}
+
+class Nightclub extends Entertainment {
+  constructor(coords, cost, duration, dance) {
+    super(coords, cost, duration);
+    this.dance = dance;
+  }
+}
+
+////////////////////////////////////////////
+// APPLICATION ARCHITECTURE
 class App {
-  
   #map;
   #mapEvent;
 
@@ -59,6 +87,11 @@ class App {
       inputDances.closest('.form__row').classList.toggle("form__row--hidden");
   
       inputActivity.closest('.form__row').classList.toggle("form__row--hidden");
+    }
+    else {
+      inputDances.closest('.form__row').classList.add("form__row--hidden");
+
+      inputActivity.closest('.form__row').classList.remove("form__row--hidden");
     }
   }
 
