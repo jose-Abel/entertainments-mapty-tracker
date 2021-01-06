ce0009383b731517cd26694f467078b2f890cc64
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-01-06 13:40:09 -0400

    Add map event listener, the map on click event to assign a value to the global mapEvent variable and display the form when clicked the map. Add event listener to form on submit to display a marker once submitted. Add event listener to the inputType to toggle classes between the inputDances and inputActivity

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.entertainments');
 const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--activities');
+const inputActivity = document.querySelector('.form__input--activities');
 const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--nightclub-dances');
-const inputElevation = document.querySelector('.form__input--price');
+const inputPrice = document.querySelector('.form__input--price');
+const inputDances = document.querySelector('.form__input--nightclub-dances');
+
+let map, mapEvent;
+
 
 if (navigator.geolocation)
 {
@@ -22,17 +25,50 @@ if (navigator.geolocation)
 
     const coords = [latitude, longitude];
 
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+
+    // Handling clicks on map
+    map.on('click', function(mapE){
+      mapEvent = mapE;
+      form.classList.remove("hidden");
+    });
 
   }, function(){
     alert("Could not get your position")
   });
-}
+};
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  // Clear Input fields
+
+  inputDuration.value = inputPrice = "";
+
+  // Display marker
+
+  const {lat, lng} = mapEvent.latlng
+
+  L.marker([lat, lng]).addTo(map)
+  .bindPopup(L.popup({
+    maxWidth: 250,
+    minWidth: 100,
+    autoClose: false,
+    closeOnClick: false,
+    className: "running-popup",
+  })).setPopupContent("Entertainment")
+  .openPopup();
+
+});
+
+inputType.addEventListener("change", function() {
+  inputDances.closest('.form__row').classList.toggle("form__row--hidden");
+
+  inputActivity.closest('.form__row').classList.toggle("form__row--hidden");
+});
+
