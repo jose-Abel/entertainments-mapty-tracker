a2ebb17478b79bfb2a37a70154e07310439c245d
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-01-06 14:24:58 -0400

    Change architecture for OOP by creating a class App in charge of adding the event listener when initialized and with the methods getPosition, loadMap and showForm, toggleDanceField and newEntertainment

--- script.js
+++ script.js
@@ -11,64 +11,77 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputPrice = document.querySelector('.form__input--price');
 const inputDances = document.querySelector('.form__input--nightclub-dances');
 
-let map, mapEvent;
+class App {
+  
+  #map;
+  #mapEvent;
 
+  constructor() {
+    this._getPosition();
 
-if (navigator.geolocation)
-{
-  navigator.geolocation.getCurrentPosition(function(position){
+    form.addEventListener('submit', this._newEntertainment.bind(this));
 
-    const {latitude} = position.coords;
-    const {longitude} = position.coords;
+    inputType.addEventListener("change", this._toggleDanceField);
+  }
 
-    console.log(`https://www.google.com.do/maps/@${latitude},${longitude}`);
+  _getPosition() {
+    if (navigator.geolocation)
+    {
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function(){
+        alert("Could not get your position")
+      });
+    };
+  }
 
-    const coords = [latitude, longitude];
+  _loadMap(position) {
+      const {latitude} = position.coords;
+      const {longitude} = position.coords;
+  
+      const coords = [latitude, longitude];
+  
+      this.#map = L.map('map').setView(coords, 13);
+  
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(this.#map);
+  
+      // Handling clicks on map
+      this.#map.on('click', this._showForm.bind(this));
+  }
 
-    map = L.map('map').setView(coords, 13);
+  _showForm(mapE){
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+  }
 
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+  _toggleDanceField() {
+    if (inputType.value === "nightclub") {
+      inputDances.closest('.form__row').classList.toggle("form__row--hidden");
+  
+      inputActivity.closest('.form__row').classList.toggle("form__row--hidden");
+    }
+  }
 
+  _newEntertainment(e){
+    e.preventDefault();
+    
+    // Clear Input fields
+    inputDuration.value = inputPrice.value = "";
+  
+    // Display marker
+    const {lat, lng} = this.#mapEvent.latlng
+    L.marker([lat, lng]).addTo(this.#map)
+    .bindPopup(L.popup({
+      maxWidth: 250,
+      minWidth: 100,
+      autoClose: false,
+      closeOnClick: false,
+      className: "running-popup",
+    })).setPopupContent("Entertainment")
+    .openPopup();
+  }
+}
 
-    // Handling clicks on map
-    map.on('click', function(mapE){
-      mapEvent = mapE;
-      form.classList.remove("hidden");
-    });
+const app = new App();
 
-  }, function(){
-    alert("Could not get your position")
-  });
-};
-
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  // Clear Input fields
-
-  inputDuration.value = inputPrice = "";
-
-  // Display marker
-
-  const {lat, lng} = mapEvent.latlng
-
-  L.marker([lat, lng]).addTo(map)
-  .bindPopup(L.popup({
-    maxWidth: 250,
-    minWidth: 100,
-    autoClose: false,
-    closeOnClick: false,
-    className: "running-popup",
-  })).setPopupContent("Entertainment")
-  .openPopup();
-
-});
-
-inputType.addEventListener("change", function() {
-  inputDances.closest('.form__row').classList.toggle("form__row--hidden");
-
-  inputActivity.closest('.form__row').classList.toggle("form__row--hidden");
-});
 
